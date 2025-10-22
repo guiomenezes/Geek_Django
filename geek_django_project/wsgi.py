15eae5544ac49ced9d77d9c5529e050778aaf51f
guiomenezes <guilherme.menezes@outlook.com>
2025-10-22 15:08:07 -0300

    Whitenoise removed and dj-static installed

--- geek_django_project/wsgi.py
+++ geek_django_project/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling, MediaCling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geek_django_project.settings')
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
